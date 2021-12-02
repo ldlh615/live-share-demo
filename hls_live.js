ab72a9968a06c06c6faa319a2b3f593399fa364f
ldlh615 <1207893654@qq.com>
2021-12-02 10:08:24 +0800

    add

--- hls_live.js
+++ hls_live.js
@@ -3,12 +3,59 @@ const fs = require("fs");
 const path = require("path");
 
 const resolve = (p) => path.resolve(__dirname, p);
+const tpl = fs.readFileSync(resolve("./source/live.m3u8"), { encoding: "utf-8" });
+const mch = tpl.match(/#EXTINF:[\d.]+/g);
+const numMch = mch.map((v) => {
+  return +v.replace("#EXTINF:", "");
+});
+const segs = numMch.reduce((a, b) => {
+  if (Array.isArray(a)) {
+    return [...a, a[a.length - 1] + b];
+  }
+  return [a, a + b];
+});
 
-// let i = 0;
-// setInterval(() => {
-//   i += 1;
-//   console.log(i);
-// }, 1000);
+// 总秒数
+const sum = ~~numMch.reduce((a, b) => {
+  return a + b;
+});
+
+// 模拟主播开播时间
+const startTime = Date.now();
+
+// 模板头
+const template = `#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-TARGETDURATION:17
+`;
+
+// 渲染完整模板
+function renderXml() {
+  let xml = template;
+  const timeGap = (Date.now() - startTime) / 1000;
+  let idx;
+  if (timeGap < sum) {
+    idx = segs.findIndex((v) => {
+      return timeGap < v;
+    });
+  } else {
+    const r = timeGap % sum;
+    idx = segs.findIndex((v) => {
+      return timeGap < r;
+    });
+  }
+  xml += `#EXT-X-MEDIA-SEQUENCE:${Math.max(idx, 0)}\n`;
+  for (let i = idx - 2; i <= idx + 2; i++) {
+    console.log(i, mch[i]);
+    if (i >= 0 && i < mch.length) {
+      // xml += `#EXTINF:0\nlive_0${mch.length + i}.ts\n`;
+    } else {
+      xml += `${mch[i]}\nlive_0${i < 10 ? "0" + i : i}.ts\n`;
+    }
+  }
+  console.log(timeGap, idx);
+  return xml;
+}
 
 const server = http.createServer((req, res) => {
   const url = req.url;
@@ -17,30 +64,20 @@ const server = http.createServer((req, res) => {
   const isTs = url.indexOf(".ts") > -1;
 
   if (isM3u8) {
-    const filePath = resolve("./source/live.m3u8");
-    const now = new Date();
-    let xml = fs.readFileSync(filePath, { encoding: "utf-8" });
-    // xml = xml.replace(/(\$DATE)/g, parseInt(now.getTime() / 100));
-    // xml = xml.replace(/(\$DATE)/g, 1619782246);
-    // xml = xml.replace(/(\$RANDOM)/g, Math.random());
-    // xml = xml.replace("$TIME", '2021-11-29T14:06:21Z');
-    // xml = xml.replace("$TIME", '2021-11-29T14:06:31Z');
-    // xml = xml.replace("$TIME", new Date(d.getTime() + 10000).toISOString());
-    // xml = xml.replace("$TIME", new Date(d.getTime() + 20000).toISOString());
-    // console.log(xml);
-    const contentBuf = Buffer.from(xml);
+    const m3u8Xml = renderXml();
+    // const contentBuf = Buffer.from(template);
     res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
-    res.setHeader("Accept-Ranges", "bytes");
-    res.setHeader("Content-Length", contentBuf.length);
-    res.setHeader("Content-Range", `bytes ${contentBuf.length - 1}/${contentBuf.length}`);
+    // res.setHeader("Accept-Ranges", "bytes");
+    // res.setHeader("Content-Length", contentBuf.length);
+    // res.setHeader("Content-Range", `bytes ${contentBuf.length - 1}/${contentBuf.length}`);
     res.writeHead(200);
-    res.write(xml);
+    res.write(m3u8Xml);
     res.end();
   } else if (isTs) {
     const str = url.split("?t=")[0];
     const filePath = resolve("./source/" + str);
-    res.setHeader("Content-Type", "video/mpeg");
-    res.setHeader("Accept-Ranges", "bytes");
+    // res.setHeader("Content-Type", "video/mpeg");
+    // res.setHeader("Accept-Ranges", "bytes");
     const rs = fs.createReadStream(filePath);
     rs.pipe(res);
   } else {
