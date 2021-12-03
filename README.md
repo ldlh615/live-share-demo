d66b06533483c002e1323765818bf359ab0260ed
ldlh615 <1207893654@qq.com>
2021-12-03 10:30:16 +0800

    feat

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # live-share-demo
+
+some demo for the share.
+
+## Topic1: What is hls and m3u8 file?
+```
+node hls_m3u8.js
+```
+
+## Topic2: How to make a live with m3u8 file?
+```
+node hls_live.js
+```
---
+++ rtmp_demo_server.js
@@ -0,0 +1,25 @@
+const Net = require("net");
+
+const port = 1234;
+
+const tcpServer = Net.createServer((socket) => {
+  console.log("client connected");
+  socket.write("hello word");
+  socket.end();
+});
+
+tcpServer.listen(port, () => {
+  console.log(`server start on localhost:${1234}, curl localhost:1234`);
+});
+
+tcpServer.on("connection", (e) => {
+  console.log('connection open');
+});
+
+tcpServer.on("error", (e) => {
+  console.error(`Node Media Rtmp Server ${e}`);
+});
+
+tcpServer.on("close", () => {
+  console.log("server close");
+});
